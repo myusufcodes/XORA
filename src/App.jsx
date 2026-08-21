f9e7b016da47513455f81158cce2da0180fda620
Muhammad yusuf <my442933@gmail.com>
2026-08-21 16:16:31 +0500

    Navigation Bar and Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+      <Header />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
