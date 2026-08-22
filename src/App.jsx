57d739dabd89dd4d3ed75a8d1289ead90a0df19d
myusufcodes <yusuf.dev.pk@gmail.com>
2026-08-22 18:28:45 +0500

    Added Footer and fixed Navbar's active styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Download from './sections/Download'
 import FAQ from './sections/FAQ'
 import Features from './sections/Features'
+import Footer from './sections/Footer'
 import Header from './sections/Header'
 import Hero from './sections/Hero'
 import Pricing from './sections/Pricing'
@@ -16,6 +17,7 @@ const App = () => {
       <FAQ />
       <Testimonials />
       <Download />
+      <Footer />
     </main>
   )
 }
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -24,9 +24,9 @@ const Header = () => {
         onClick={() => {setIsOpen(false)}}
             to={title}
             offset={-100}
+            activeStyle={{ color: '#C8EA80' }}
             spy
             smooth
-            activeClass='nav-active'
             className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'
         >
             {title}
